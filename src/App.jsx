ea76a00770bf33164d40b347d73fad4d4f7a03e0
Monito <daniel142giraldo@gmail.com>
2026-08-26 11:19:03 -0500

    Modificacion en app

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+import Formularioiniciosesion from "./components/Formularioiniciosesion.jsx";
+import Formularioregistro from "./components/Formularioregistro.jsx";
+import Listadoinmuebles from "./components/Listadoinmueble.jsx";
+import GestionInmuebles from "./components/GestionInmuebles.jsx";
+
+import "./App.css";
+
+function App() {
+  const [pagina, setPagina] = useState("login");
+
+  const [rol, setRol] = useState("");
+
+  const [persona, setPersona] = useState({
+    nombre: "",
+    apellido: "",
+    correo: "",
+    telefono: "",
+    contrasena: "",
+  });
+
+  return (
+    <>
+     
+      {pagina === "login" && (
+        <Formularioiniciosesion
+          irARegistro={() => setPagina("registro")}
+
+          iniciarSesion={(tipoUsuario) => {
+            setRol(tipoUsuario);
+
+            if (tipoUsuario === "admin") {
+              setPagina("gestion");
+            } else {
+              setPagina("inmuebles");
+            }
+          }}
+        />
+      )}
+
+   
+      {pagina === "registro" && (
+        <Formularioregistro
+          persona={persona}
+          setPersona={setPersona}
+
+          anterior={() => setPagina("login")}
+
+          siguiente={() => {
+            alert("¡Registro exitoso!");
+            setPagina("login");
+          }}
+        />
+      )}
+
+      {pagina === "inmuebles" && (
+        <ListadoInmuebles />
+      )}
+
+      {pagina === "gestion" && (
+        <GestionInmuebles />
+      )}
+    </>
+  );
+}
+
+export default App;
